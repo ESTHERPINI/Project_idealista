fe8e368d1b910b12d4dc86ae675391c8abb23901
ESTHERPINI <pinillalorenzoesther@hotmail.com>
2021-07-16 14:05:40 +0200

    more streamlit

--- used_model.py
+++ used_model.py
@@ -17,6 +17,8 @@ from sklearn.metrics import confusion_matrix , accuracy_score, precision_score, 
 from PIL import Image
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge, Lasso
+import sklearn.metrics as metrics
+from sklearn.linear_model import BayesianRidge
 
 st.title('ML MODELS FOR HOUSE PRICE PREDICTION')
 #title image
@@ -123,139 +125,224 @@ st.header('Model Fitting')
 
 X_train,X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=7)
 
-alg = ['KneighborsRegressor','LinearRegression','Ridge']
+alg = ['KneighborsRegressor','LinearRegression','Ridge', 'Bayesian','Lasso']
 predictor = st.selectbox('Which algorithm?', alg)
+
 
 
 #Linear Regression
 if predictor=='LinearRegression':
-
     LR=LinearRegression()
     LR.fit(X_train, y_train)
-    y_pred=LR.predict(X_train)    
-    
-testing_score=[]
-training_score=[]
-cv_results_rms = []
-R2=[]
-adj_R2=[]
-MAE=[]
-MSE=[]
-RMSE=[]
-LR_training_score= LR.score(X_train , y_train)*100
-training_score.append(LR_training_score.mean())
-st.write("Training Accuracy: ", LR.score(X_train , y_train)*100)
+    y_pred=LR.predict(X_train) 
 
-LR_testing_score=LR.score(X_train , y_train)*100
-testing_score.append(LR_testing_score.mean())
-st.write("Testing Accuracy:" , LR.score(X_test, y_test)*100)
+    testing_score=[]
+    training_score=[]
+    cv_results_rms = []
+    R2=[]
+    adj_R2=[]
+    MAE=[]
+    MSE=[]
+    RMSE=[]   
 
-cv_score = cross_val_score(LR, X_train,y_train,scoring="neg_root_mean_squared_error", cv=10)
-cv_results_rms.append(cv_score.mean())
-st.write("cross_val ","%s: %f " % (LR, cv_score.mean()))
-
-pred =LR.predict(X_test)
-
-#R2
-R2_metrics=r2_score(y_test, pred)
-R2.append(R2_metrics.mean())
-st.write("R2 ","%s: %f " % (LR,  R2_metrics.mean()))
-
-#MAE
-#MAE_metrics=mean_absolute_error(y_test, pred)
-#MAE.append(MAE_metrics.mean())
-#st.write("MAE", "%s: %f " % (KN,  MAE_metrics.mean()))  
-
-#MSE    
-MSE_metrics=mean_squared_error(y_test, pred)
-MSE.append(MSE_metrics.mean())
-st.write("MSE ","%s: %f " % (LR,  MSE_metrics.mean()))
+    #TRAINING ACC SCORE
+    LR_traing_score=LR.score(X_train, y_train)*100
+    training_score.append(LR_traing_score.mean())
+    st.write("Training Accuracy:", LR.score(X_train,y_train)*100)
+    #TESTING ACC SCORE
+    LR_testing_score=LR.score(X_test, y_test)*100
+    testing_score.append(LR_testing_score.mean())
+    st.write("Testing Accuracy:", LR.score(X_test, y_test)*100)
 
 
 
-#Ridge
+    #X_test predict
+    cv_score = cross_val_score(LR, X_train,y_train,scoring="neg_root_mean_squared_error", cv=10)
+    cv_results_rms.append(cv_score.mean())
+    st.write("cross_val ","%s: %f " % ('LR', cv_score.mean()))
+
+    pred = LR.predict(X_test)
+
+    #R2
+    R2_metrics=metrics.r2_score(y_test, pred)
+    R2.append(R2_metrics.mean())
+    st.write("R2 ","%s: %f " % ('LR',  R2_metrics.mean()))
+
+    #adj_R2
+    adj_R2_metrics=(1 - (1-metrics.r2_score(y_test, pred))*(len(y_test)-1)/(len(y_test)-X_test.shape[1]-1))
+    adj_R2.append(adj_R2_metrics.mean())
+    st.write("adj_R2 ","%s: %f " % ('LR',  adj_R2_metrics.mean()))   
+
+    #MAE
+    MAE_metrics=metrics.mean_absolute_error(y_test, pred)
+    MAE.append(MAE_metrics.mean())
+    st.write("MAE", "%s: %f " % ('LR',  MAE_metrics.mean()))  
+
+    #MSE    
+    MSE_metrics=metrics.mean_squared_error(y_test, pred)
+    MSE.append(MSE_metrics.mean())
+    st.write("MSE ","%s: %f " % ('LR',  MSE_metrics.mean()))
+
+    #RMSE
+    RMSE_metrics=np.sqrt(metrics.mean_squared_error(y_test, pred))
+    RMSE.append(RMSE_metrics.mean())
+    st.write("RMSE ","%s: %f " % ('LR', RMSE_metrics.mean()))    
+
+
+
+#RIDGE
 if predictor=='Ridge':
-    ridge=Ridge()
+    ridge=Ridge(alpha=0.5)
     ridge.fit(X_train, y_train)
-    y_pred=ridge.predict(X_train)    
+    y_pred=ridge.predict(X_train) 
+
+    testing_score=[]
+    training_score=[]
+    cv_results_rms = []
+    R2=[]
+    adj_R2=[]
+    MAE=[]
+    MSE=[]
+    RMSE=[] 
+
+    ridge_traing_score=ridge.score(X_train, y_train)*100
+    training_score.append(ridge_traing_score.mean())
+    st.write("Training Accuracy: ", ridge.score(X_train , y_train)*100)
+
+    ridge_testing_score=ridge.score(X_test, y_test)*100
+    testing_score.append(ridge_testing_score.mean())
+    st.write("Testing Accuracy:" , ridge.score(X_test, y_test)*100)
+
+    cv_score = cross_val_score(ridge, X_train,y_train,scoring="neg_root_mean_squared_error", cv=10)
+    cv_results_rms.append(cv_score.mean())
+    st.write("cross_val ","%s: %f " % ('ridge', cv_score.mean()))
+
+    pred = ridge.predict(X_test)
+
+    #R2
+    R2_metrics=metrics.r2_score(y_test, pred)
+    R2.append(R2_metrics.mean())
+    st.write("R2 ","%s: %f " % (ridge,  R2_metrics.mean()))
+
+    #adj_R2
+    adj_R2_metrics=(1 - (1-metrics.r2_score(y_test, pred))*(len(y_test)-1)/(len(y_test)-X_test.shape[1]-1))
+    adj_R2.append(adj_R2_metrics.mean())
+    st.write("adj_R2 ","%s: %f " % (ridge,  adj_R2_metrics.mean()))   
+
+    #MAE
+    MAE_metrics=metrics.mean_absolute_error(y_test, pred)
+    MAE.append(MAE_metrics.mean())
+    st.write("MAE", "%s: %f " % (ridge,  MAE_metrics.mean()))  
+
+    #MSE    
+    MSE_metrics=metrics.mean_squared_error(y_test, pred)
+    MSE.append(MSE_metrics.mean())
+    st.write("MSE ","%s: %f " % (ridge,  MSE_metrics.mean()))
+
+    #RMSE
+    RMSE_metrics=np.sqrt(metrics.mean_squared_error(y_test, pred))
+    RMSE.append(RMSE_metrics.mean())
+    st.write("RMSE ","%s: %f " % (ridge, RMSE_metrics.mean()))    
+
+if predictor=='Lasso':
+     
+    lasso = Lasso(alpha = 0.01)
+    lasso.fit(X_train, y_train)
+    y_pred=lasso.predict(X_train)
+
+    testing_score=[]
+    training_score=[]
+    cv_results_rms = []
+    R2=[]
+    adj_R2=[]
+    MAE=[]
+    MSE=[]
+    RMSE=[] 
     
-testing_score=[]
-training_score=[]
-cv_results_rms = []
-R2=[]
-adj_R2=[]
-MAE=[]
-MSE=[]
+    lasso_training_score=lasso.score(X_train, y_train)*100
+    training_score.append(lasso_training_score.mean())
+    st.write("Training Accuracy: ", lasso.score(X_train , y_train)*100)
 
-ridge_training_score=ridge.score(X_train , y_train)*100
-training_score.append(ridge_training_score.mean())
-st.write("Training Accuracy: ", ridge.score(X_train , y_train)*100)
+    cv_score = cross_val_score(lasso, X_train,y_train,scoring="neg_root_mean_squared_error", cv=10)
+    cv_results_rms.append(cv_score.mean())
+    st.write("cross_val ","%s: %f " % ('Lasso', cv_score.mean()))
 
-ridge_testing_score=ridge.score(X_train , y_train)*100
-testing_score.append(ridge_testing_score.mean())
-st.write("Testing Accuracy:" , ridge.score(X_test, y_test)*100)
+    pred = lasso.predict(X_test)
 
-cv_score = cross_val_score(ridge, X_train,y_train,scoring="neg_root_mean_squared_error", cv=10)
-cv_results_rms.append(cv_score.mean())
-st.write("cross_val ","%s: %f " % (ridge, cv_score.mean()))
+    #R2
+    R2_metrics=metrics.r2_score(y_test, pred)
+    R2.append(R2_metrics.mean())
+    st.write("R2 ","%s: %f " % (lasso,  R2_metrics.mean()))
 
-pred =ridge.predict(X_test)
+    #adj_R2
+    adj_R2_metrics=(1 - (1-metrics.r2_score(y_test, pred))*(len(y_test)-1)/(len(y_test)-X_test.shape[1]-1))
+    adj_R2.append(adj_R2_metrics.mean())
+    st.write("adj_R2 ","%s: %f " % (lasso,  adj_R2_metrics.mean()))   
 
-#R2
-R2_metrics=r2_score(y_test, pred)
-R2.append(R2_metrics.mean())
-st.write("R2 ","%s: %f " % (ridge,  R2_metrics.mean()))
+    #MAE
+    MAE_metrics=metrics.mean_absolute_error(y_test, pred)
+    MAE.append(MAE_metrics.mean())
+    st.write("MAE", "%s: %f " % (lasso,  MAE_metrics.mean()))  
 
-#MAE
-#MAE_metrics=mean_absolute_error(y_test, pred)
-#MAE.append(MAE_metrics.mean())
-#st.write("MAE", "%s: %f " % (ridge,  MAE_metrics.mean()))  
+    #MSE    
+    MSE_metrics=metrics.mean_squared_error(y_test, pred)
+    MSE.append(MSE_metrics.mean())
+    st.write("MSE ","%s: %f " % (lasso,  MSE_metrics.mean()))
 
-#MSE    
-MSE_metrics=mean_squared_error(y_test, pred)
-MSE.append(MSE_metrics.mean())
-st.write("MSE ","%s: %f " % (ridge,  MSE_metrics.mean()))
+    #RMSE
+    RMSE_metrics=np.sqrt(metrics.mean_squared_error(y_test, pred))
+    RMSE.append(RMSE_metrics.mean())
+    st.write("RMSE ","%s: %f " % (lasso, RMSE_metrics.mean()))  
 
+if predictor=='KneighborsRegressor':     
+    KN = KNeighborsRegressor()
+    KN.fit(X_train, y_train)
+    y_pred=KN.predict(X_train)  
 
-#KneighborsRegressor
-if predictor=='KneighborsRegressor':
-    kn=KneighborsRegressor()
-    kn.fit(X_train, y_train)
-    y_pred=kn.predict(X_train)    
+    testing_score=[]
+    training_score=[]
+    cv_results_rms = []
+    R2=[]
+    adj_R2=[]
+    MAE=[]
+    MSE=[]
+    RMSE=[]
     
-testing_score=[]
-training_score=[]
-cv_results_rms = []
-R2=[]
-adj_R2=[]
-MAE=[]
-MSE=[]
+    KN_training_score=KN.score(X_train , y_train)*100
+    training_score.append(KN_training_score.mean())
+    st.write("Training Accuracy: ", KN.score(X_train , y_train)*100)
 
-kn_training_score=kn.score(X_train , y_train)*100
-training_score.append(kn_training_score.mean())
-st.write("Training Accuracy: ", kn.score(X_train , y_train)*100)
+    KN_testing_score=KN.score(X_train , y_train)*100
+    testing_score.append(KN_testing_score.mean())
+    st.write("Testing Accuracy:" , KN.score(X_test, y_test)*100)
 
-ridge_testing_score=kn.score(X_train , y_train)*100
-testing_score.append(kn_testing_score.mean())
-st.write("Testing Accuracy:" , kn.score(X_test, y_test)*100)
+    cv_score = cross_val_score(KN, X_train,y_train,scoring="neg_root_mean_squared_error", cv=10)
+    cv_results_rms.append(cv_score.mean())
+    st.write("cross_val ","%s: %f " % ('KN', cv_score.mean()))
 
-cv_score = cross_val_score(kn, X_train,y_train,scoring="neg_root_mean_squared_error", cv=10)
-cv_results_rms.append(cv_score.mean())
-st.write("cross_val ","%s: %f " % (kn, cv_score.mean()))
+    pred =KN.predict(X_test)
 
-pred =kn.predict(X_test)
+    #R2
+    R2_metrics=metrics.r2_score(y_test, pred)
+    R2.append(R2_metrics.mean())
+    st.write("R2 ","%s: %f " % (KN,  R2_metrics.mean()))
 
-#R2
-R2_metrics=r2_score(y_test, pred)
-R2.append(R2_metrics.mean())
-st.write("R2 ","%s: %f " % (ridge,  R2_metrics.mean()))
+    #adj_R2
+    adj_R2_metrics=(1 - (1-metrics.r2_score(y_test, pred))*(len(y_test)-1)/(len(y_test)-X_test.shape[1]-1))
+    adj_R2.append(adj_R2_metrics.mean())
+    st.write("adj_R2 ","%s: %f " % (KN,  adj_R2_metrics.mean()))   
 
-#MAE
-#MAE_metrics=mean_absolute_error(y_test, pred)
-#MAE.append(MAE_metrics.mean())
-#st.write("MAE", "%s: %f " % (ridge,  MAE_metrics.mean()))  
+    #MAE
+    MAE_metrics=metrics.mean_absolute_error(y_test, pred)
+    MAE.append(MAE_metrics.mean())
+    st.write("MAE", "%s: %f " % (KN,  MAE_metrics.mean()))  
 
-#MSE    
-MSE_metrics=mean_squared_error(y_test, pred)
-MSE.append(MSE_metrics.mean())
-st.write("MSE ","%s: %f " % (kn,  MSE_metrics.mean()))
+    #MSE    
+    MSE_metrics=metrics.mean_squared_error(y_test, pred)
+    MSE.append(MSE_metrics.mean())
+    st.write("MSE ","%s: %f " % (KN,  MSE_metrics.mean()))
+
+    #RMSE
+    RMSE_metrics=np.sqrt(metrics.mean_squared_error(y_test, pred))
+    RMSE.append(RMSE_metrics.mean())
+    st.write("RMSE ","%s: %f " % (KN, RMSE_metrics.mean()))    
